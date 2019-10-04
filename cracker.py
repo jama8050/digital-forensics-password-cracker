e1949979515a6b826cdca3fddd25ba3b888bca76
Jacob Malcy <jama8050@colorado.edu>
2019-10-04 17:01:06 -0600

    Added more comments

--- cracker.py
+++ cracker.py
@@ -67,19 +67,32 @@ def brute_force(given_hash, used_dict, limit=-1):
     # Infinite loop if no limit, else bounded by limit
     while (brute_length < limit and limit != -1) or (limit == -1):
         brute_length += 1
-        print("Brute-force cracking with passwords of {} length".format(brute_length))
+        print("Brute-force cracking with passwords of length {}".format(brute_length))
 
+        # Size before we increase list size
         prior_size = len(last_combos)
+
+        # ['a', 'b']*3 becomes ['a', 'b', 'a', 'b', 'a', 'b']
         last_combos *= dict_len
 
+        # index in range [0, len(last_combos)] (inclusive)
         i = 0
+
+        # For individual character in available characters list (used_dict)
         for c in used_dict:
+            # Append character c to "prior_size" entries in the list
             for j in range(0, prior_size):
+                # Increment number of attempted hashes
                 n_tries += 1
+
+                # Append character c to specific combination at index i
                 last_combos[i] += c
+
+                # if new combo, located at last_combos[i], hashes to desired value, return
                 if md5(last_combos[i]).hexdigest() == given_hash:
                     return n_tries, (time() - start_time), last_combos[i]
                 else:
+                    # Move to next element in the last_combos list
                     i += 1
 
     # Couldn't find the hash after "i" tries, return False
@@ -87,6 +100,7 @@ def brute_force(given_hash, used_dict, limit=-1):
 
 
 if __name__ == "__main__":
+    # encoding we'll be using to encode characters with that will be hashed
     encoding = "ascii"
     parser = ArgumentParser(description="Python3 md5 cracker (default brute force)",
                                      epilog="Created by Jacob Malcy")
@@ -104,6 +118,7 @@ if __name__ == "__main__":
     # Parse cmd arguments
     parsed = parser.parse_args()
 
+    # Determine what type of attack we'll be performing
     attack_type = "Dictionary" if parsed.wordlist else "Brute-force"
     hash_to_crack = parsed.hash
 
@@ -114,7 +129,6 @@ if __name__ == "__main__":
                                                         wordlist)
     else:
         # Brute force mode
-        # [b'a', b'b', b'c'],[c.encode(encoding) for c in printable[:-2]]
         number_of_hashes, elapsed, result = brute_force(hash_to_crack,
                                                         [c.encode(encoding) for c in ascii_lowercase])
 
