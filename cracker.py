34a2e408da538241624936b65b84c271f597542b
Jacob Malcy <jama8050@colorado.edu>
2019-10-04 11:05:01 -0600

    Considerable changes for performance
    I should have done more commits than I did...

--- cracker.py
+++ cracker.py
@@ -1,15 +1,10 @@
-import hashlib
-import argparse
-import time
+from hashlib import md5
+from _md5 import md5
+from argparse import ArgumentParser
+from time import time
 
 # A string of all printable ASCII characters, with some of the whitespace ones removed
-from string import ascii_letters, digits, punctuation
-# Dictionary used for brute forcing
-b_dict = list(ascii_letters)
-b_dict.extend(digits)
-b_dict.extend(punctuation)
-b_dict.extend([' ', '\t'])
-
+from string import ascii_lowercase
 
 # TODO: Implement multithreading?
 # TODO: Implement GPU support?
@@ -23,8 +18,8 @@ b_dict.extend([' ', '\t'])
 
 # Reads the lines from the given file and returns list of all the lines
 def read_wordlist(file_argument):
-    with open(file_argument) as reader:
-        lines = [x.strip("\n") for x in reader.readlines()]
+    with open(file_argument, 'rb') as reader:
+        lines = [line.strip() for line in reader.readlines() if len(line.strip()) > 0]
     return lines
 
 
@@ -37,21 +32,19 @@ def read_wordlist(file_argument):
 #       password if match found
 def dict_attack(given_hash, used_dict):
     # Number of hashes computed in total
-    number_of_hashes = 0
-    start_time = time.time()
+    n_tries = 0
+    start_time = time()
 
     # For every word in the dictionary
     for word in used_dict:
-        number_of_hashes += 1
-        # Hash the word in the wordlist
-        hashed_word = give_hash(word)
+        n_tries += 1
 
         # If found hash, return the word from the wordlist that worked
-        if hashed_word == given_hash:
-            return number_of_hashes,  (time.time() - start_time), word
+        if md5(word).hexdigest() == given_hash:
+            return n_tries, (time() - start_time), word
 
     # If the password couldn't be found, return False
-    return number_of_hashes, (time.time() - start_time), False
+    return n_tries, (time() - start_time), False
 
 
 # Given the md5 hash, a dictionary to use, and a limit on the length of the password (optional), brute force the hash.
@@ -62,54 +55,63 @@ def dict_attack(given_hash, used_dict):
 #       False if no match found
 #       password if match found
 def brute_force(given_hash, used_dict, limit=-1):
-    last_combos = ['']
-
+    dict_len = len(used_dict)
     # How many hashes in total have been tried
-    number_of_hashes = 0
+    n_tries = 0
 
     # Current length of the hashes
     brute_length = 0
+
+    # List of the hashes from the previous iteration of the while loop
+    last_combos = [b'']
+
+    # Time we start cracking
+    start_time = time()
+
     # Infinite loop if no limit, else bounded by limit
-    start_time = time.time()
     while (brute_length < limit and limit != -1) or (limit == -1):
         brute_length += 1
-        # Combinations we've done so far
-        current_combos = []
+        print("Brute-force cracking with passwords of {} length".format(brute_length))
 
-        # For every character in the provided dictionary
+        prior_size = len(last_combos)
+        last_combos *= dict_len
+
+        i = 0
         for c in used_dict:
-            # For every combination that was generated last iteration
-            for combo in last_combos:
-                number_of_hashes += 1
-
-                # Generate and hash the current combination
-                immediate_combo = c + combo
-                current_hash = give_hash(immediate_combo)
-
-                # print('Trying hash "{}"\t{}'.format(immediate_combo, current_hash))
-                if current_hash == given_hash:
-                    # Desired clear-text found, return it and the number of hashes we did before completion
-                    return number_of_hashes, (time.time() - start_time), immediate_combo
+            for j in range(0, prior_size):
+                n_tries += 1
+                last_combos[i] += c
+                if md5(last_combos[i]).hexdigest() == given_hash:
+                    return n_tries, (time() - start_time), last_combos[i]
                 else:
-                    # immediate_combo is not the desired clear-text, add it to the current_combos for the next iteration
-                    current_combos.append(immediate_combo)
+                    i += 1
 
-        # Loop completed, replace old "last_combos" with "current_combos" for next iteration
-        last_combos = current_combos
+        # Im just going to keep this here. It's from a previous attempt that I forgot to commit
+        # for i in range(0, lc_size):
+        #     immediate_combo = last_combos[i]
+        #     for c in used_dict:
+        #         n_tries += 1
+        #         sub_h = md5(immediate_combo + c).hexdigest()
+        #         print((immediate_combo + c).decode("ascii"))
+        #
+        #         # print('Trying hash "{}"\t{}'.format(immediate_combo, current_hash))
+        #         if sub_h == given_hash:
+        #             # Desired clear-text found, return it and the number of hashes we did before completion
+        #             return n_tries, (time() - start_time), (immediate_combo + c)
+        #         else:
+        #             # immediate_combo is not the desired clear-text, add it to the current_combos for the next iteration
+        #             last_combos[i] += c
+        #
+        # # Loop completed, replace old "last_combos" with "current_combos" for next iteration
+        # #last_combos = current_combos
 
     # Couldn't find the hash after "i" tries, return False
-    return number_of_hashes, (time.time() - start_time), False
+    return n_tries, (time() - start_time), False
 
 
-# Hash a given string
-def give_hash(s):
-    current_hash = hashlib.md5()
-    current_hash.update(str.encode(s))
-    return current_hash.hexdigest()
-
-
-def main():
-    parser = argparse.ArgumentParser(description="Python3 md5 cracker (default brute force)",
+if __name__ == "__main__":
+    encoding = "ascii"
+    parser = ArgumentParser(description="Python3 md5 cracker (default brute force)",
                                      epilog="Created by Jacob Malcy")
 
     # Add wordlist parameter, optional
@@ -126,14 +128,18 @@ def main():
     parsed = parser.parse_args()
 
     attack_type = "Dictionary" if parsed.wordlist else "Brute-force"
+    hash_to_crack = parsed.hash
 
     # If given wordlist, use it
     if attack_type == "Dictionary":
         wordlist = read_wordlist(parsed.wordlist)
-        number_of_hashes, elapsed, result = dict_attack(parsed.hash, wordlist)
+        number_of_hashes, elapsed, result = dict_attack(hash_to_crack,
+                                                        wordlist)
     else:
         # Brute force mode
-        number_of_hashes, elapsed, result = brute_force(parsed.hash, b_dict)
+        # [b'a', b'b', b'c'],[c.encode(encoding) for c in printable[:-2]]
+        number_of_hashes, elapsed, result = brute_force(hash_to_crack,
+                                                        [c.encode(encoding) for c in ascii_lowercase])
 
     print("Attacked hash for {} seconds.".format(elapsed))
     print("Tried {} different passwords.".format(number_of_hashes))
@@ -142,8 +148,5 @@ def main():
     if result is False:
         print(attack_type, "attack failed to find a match.")
     else:
-        print('Match found: "{}"'.format(result))
+        print('Match found: "{}"'.format(result.decode('ascii')))
 
-
-if __name__ == "__main__":
-    main()
